37c6b68635b5f5dad53ee5fd95cad2a4650f79fc
Tripleo envybox <oluoluolu@gmail.com>
2020-05-03 04:14:51 -0400

    formatting

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
+# https://medium.com/free-code-camp/how-to-build-a-web-app-using-pythons-flask-and-google-app-engine-52b1bb82b221
 import requests
 
 API_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXX'
@@ -5,10 +6,11 @@ API_URL = ('http://api.openweathermap.org/data/2.5/weather?q={}&mode=json&units=
 
 
 def query_api(city):
-    try:
-        print(API_URL.format(city, API_KEY))
-        data = requests.get(API_URL.format(city, API_KEY)).json()
-    except Exception as exc:
-        print(exc)
-#    data = None
-    return data
+	try:
+		print(API_URL.format(city, API_KEY))
+		data = requests.get(API_URL.format(city, API_KEY)).json()
+		print (100, data)
+	except Exception as exc:
+		print(exc)
+#	data = None
+	return data
